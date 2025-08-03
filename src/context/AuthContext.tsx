3e1c5fecb4f2abf49b0609fec71bf73d48e06a5a
Douglas <ftsgmr@gmail.com>
2025-08-03 08:41:24 -0300

    update

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -32,7 +32,6 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 		const unsubscribe = onAuthStateChanged(
 			firebaseAuth,
 			(user) => {
-				console.log(user);
 				if (user) {
 					setAuthSate({
 						user: {
