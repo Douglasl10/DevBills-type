a8832f5741591a100a99cb8772573745c1f05ffe
Douglas <ftsgmr@gmail.com>
2025-08-03 08:45:25 -0300

    up

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -3,7 +3,7 @@ import { api } from "./api";
 
 export const getCategories = async (): Promise<Category[]> => {
 	try {
-		const response = await api.get("/api/categories");
+		const response = await api.get("/categories");
 		return response.data;
 	} catch (error) {
 		console.error(error);
