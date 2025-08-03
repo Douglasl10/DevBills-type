244f48a6e6a12b14931a561cf15c2a32a9dd09c4
Douglas <ftsgmr@gmail.com>
2025-08-03 09:26:30 -0300

    up

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,7 +8,6 @@ export const api: AxiosInstance = axios.create({
 	baseURL: import.meta.env.VITE_API_URL,
 	timeout: 10000,
 });
-console.log(import.meta.env.VITE_API_URL);
 
 api.interceptors.request.use(
 	async (
