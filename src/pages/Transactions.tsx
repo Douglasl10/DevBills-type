d3e2db028f81d072bea5597f242d884d93ed04da
Douglas <ftsgmr@gmail.com>
2025-09-28 22:36:40 -0300

    new-update

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -6,7 +6,7 @@ import {
 	Search,
 	Trash2,
 } from "lucide-react";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import MonthYearSelect from "../components/MonthYearSelect";
 import { useEffect, useState } from "react";
 import Input from "../components/Input";
@@ -54,7 +54,6 @@ const Transactios = () => {
 			await deleteTransaction(id);
 			toast.success("Sucesso ao deletar transação");
 
-			// garante sincronização dos dois estados
 			setTransactions((prev) => prev.filter((t) => t.id !== id));
 			setFilteredTransactions((prev) => prev.filter((t) => t.id !== id));
 		} catch (err) {
@@ -134,20 +133,6 @@ const Transactios = () => {
 							Tentar Novamente
 						</Button>
 					</div>
-				) : transactions?.length === 0 ? (
-					<div className="py-12 items-center">
-						<p className=" text-gray-500 mb-4 text-center">
-							Nenhuma transação encontrada
-						</p>
-						<Link
-							to="/transacoes/nova"
-							className=" w-fit mx-auto mt-6 bg-primary-500 text-[#051626] font-semibold px-4 py-2.5 rounded-xl flex items-center 
-                    justify-center hover:bg-primary-600 transition-all"
-						>
-							<Plus className="mr-2 w-4 h-4" />
-							Adicionar Transação
-						</Link>
-					</div>
 				) : (
 					<div className="overflow-x-auto">
 						<table className="divide-y divide-gray-700 min-h-full w-full">
@@ -171,72 +156,77 @@ const Transactios = () => {
 								</tr>
 							</thead>
 							<tbody className="divide-y divide-gray-700">
-								{filteredTransactions.map((transaction) => (
-									<tr
-										key={
-											transaction.id ||
-											`${transaction.description}-${transaction.date}`
-										}
-										className="hover:bg-gray-800"
-									>
-										<td className="px-3 py-4 text-gray-400 whitespace-nowrap">
-											<div className="flex items-center">
-												<div className="mr-2">
-													{transaction.type === TransactionType.INCOME ? (
-														<ArrowUp className="w-4 h-4 text-primary-600" />
-													) : (
-														<ArrowDown className="w-4 h-4 text-red-600" />
-													)}
-												</div>
-												<span className="font-medium text-gray-50">
-													{transaction.description}
-												</span>
-											</div>
-										</td>
-										<td className="px-3 py-4 text-sm text-gray-400 whitespace-nowrap">
-											{formatDate(transaction.date)}
-										</td>
-										<td className="px-3 py-4 text-sm text-gray-400 whitespace-nowrap">
-											<div className="flex items-center">
-												<div
-													className="mr-2 w-2 h-2 rounded-full"
-													style={{
-														backgroundColor:
-															transaction.category?.color || "#999",
-													}}
-												/>
-												<span className="text-sm text-gray-400">
-													{transaction.category?.name ?? "Sem categoria"}
-												</span>
-											</div>
-										</td>
-										<td className="px-6 py-4 text-sm text-gray-400 whitespace-nowrap">
-											<span
-												className={`${
-													transaction.type === TransactionType.INCOME
-														? "text-green-600"
-														: "text-red-700"
-												}`}
-											>
-												{formatCurrency(transaction.amount)}
-											</span>
-										</td>
-										<td className="px-3 py-4 whitespace-nowrap">
-											<button
-												type="button"
-												onClick={() => confirmDelete(transaction.id)}
-												className="text-red-400 hover:text-red-700 rounded-full cursor-pointer"
-												disabled={deleteId === transaction.id}
-											>
-												{deleteId === transaction.id ? (
-													<span className="inline-block w-4 h-4 border-2 border-red-500 rounded-full border-t-transparent animate-spin" />
-												) : (
-													<Trash2 className="w-4 h-4" />
-												)}
-											</button>
+								{filteredTransactions.length === 0 ? (
+									<tr>
+										<td colSpan={5} className="text-center py-6 text-gray-500">
+											Nenhuma transação encontrada
 										</td>
 									</tr>
-								))}
+								) : (
+									filteredTransactions.map((transaction) => (
+										<tr
+											key={`tx-${transaction.id}`}
+											className="hover:bg-gray-800"
+										>
+											<td className="px-3 py-4 text-gray-400 whitespace-nowrap">
+												<div className="flex items-center">
+													<div className="mr-2">
+														{transaction.type === TransactionType.INCOME ? (
+															<ArrowUp className="w-4 h-4 text-primary-600" />
+														) : (
+															<ArrowDown className="w-4 h-4 text-red-600" />
+														)}
+													</div>
+													<span className="font-medium text-gray-50">
+														{transaction.description}
+													</span>
+												</div>
+											</td>
+											<td className="px-3 py-4 text-sm text-gray-400 whitespace-nowrap">
+												{formatDate(transaction.date)}
+											</td>
+											<td className="px-3 py-4 text-sm text-gray-400 whitespace-nowrap">
+												<div className="flex items-center">
+													<div
+														className="mr-2 w-2 h-2 rounded-full"
+														style={{
+															backgroundColor:
+																transaction.category?.color || "#999",
+														}}
+													/>
+													<span className="text-sm text-gray-400">
+														{transaction.category?.name ?? "Sem categoria"}
+													</span>
+												</div>
+											</td>
+											<td className="px-6 py-4 text-sm text-gray-400 whitespace-nowrap">
+												<span
+													className={`${
+														transaction.type === TransactionType.INCOME
+															? "text-green-600"
+															: "text-red-700"
+													}`}
+												>
+													{formatCurrency(transaction.amount)}
+												</span>
+											</td>
+											<td className="px-3 py-4 whitespace-nowrap">
+												<button
+													type="button"
+													onClick={() => confirmDelete(transaction.id)}
+													className="text-red-400 hover:text-red-700 rounded-full cursor-pointer"
+													disabled={deleteId === transaction.id}
+												>
+													{deleteId === transaction.id ? (
+														<span className="inline-block w-4 h-4 border-2 border-red-500 rounded-full border-t-transparent animate-spin" />
+													) : (
+														<Trash2 className="w-4 h-4" />
+													)}
+												</button>
+											</td>
+										</tr>
+									))
+								)}
 							</tbody>
 						</table>
 					</div>
